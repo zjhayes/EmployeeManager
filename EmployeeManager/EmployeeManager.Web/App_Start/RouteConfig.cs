53023f433937a51dd2fc7242d9ff7ec4989a903c
ChzWhz <zachary.j.hayes@gmail.com>
2018-10-17 19:56:42 -0500

    Week 7 Add Layout page
    Added _Layout.cshtml and _ViewStart.cshtml to create template for all views.

--- EmployeeManager/EmployeeManager.Web/App_Start/RouteConfig.cs
+++ EmployeeManager/EmployeeManager.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace EmployeeManager.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
